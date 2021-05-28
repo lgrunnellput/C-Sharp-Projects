d86c4978dc8ea659a5a2051cc7b03ee8615ec037
lgrunnellput <83829445+lgrunnellput@users.noreply.github.com>
2021-05-28 08:13:40 +0100

    Add files via upload
    Calculate your BMI and know what it means. It calculates from metric to imperial units.

---
+++ BMICalculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace BMI_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter your height in inches:");
+            double height = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter your weight in pounds:");
+            double weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your bmi is:" +);
+            Console.ReadLine();
+
+        }//end of main
+
+
+        public static double convertToMeters(double inches)
+        {
+            double meters;
+
+            meters = (inches * 2.54) / 100;
+
+            return meters;
+
+        }
+
+        public static double convertToKg(double pounds)
+        {
+            double kg;
+
+            kg = pounds / 2.2;
+
+            return kg;
+        }
+
+        public static double calcBMI(double weight, double height)
+        { 
+            double bmi;
+
+            bmi = weight / (height * height);
+
+            return bmi;
+
+        }
+    }
+}
