5e0e05c929b2451d81719abd8bb772b413f29145
lgrunnellput <83829445+lgrunnellput@users.noreply.github.com>
2021-05-28 08:12:13 +0100

    Add files via upload
    Calculate what bus(es) a school should take for a school trip to have the smallest number of buses used at a lower cost.

---
+++ Bus Trip Challenge.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Bus_Trip_Challenge_Question
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Homework: Bus challenge question
+
+            Console.WriteLine("Would you like a medium or a large bus?");
+
+            string busType = Console.ReadLine();
+
+            Console.WriteLine("How many pupils on your bus trip?");
+
+            int pupilNumber = Int32.Parse(Console.ReadLine());
+
+            int teacherNumber = (pupilNumber / 10) + 1;
+
+            int totalNumber = pupilNumber + teacherNumber;
+
+            Console.WriteLine("The total number of pupils and teachers on your trip is " + totalNumber);
+
+            int busCapacity;
+
+            if (busType == "med")
+            {
+                busCapacity = 30;
+            }
+            else
+            {
+                busCapacity = 60;
+            }
+
+            int busNumber = totalNumber / busCapacity;
+
+            if (totalNumber % busCapacity == 0)
+            {
+                busNumber = totalNumber / busCapacity;
+            }
+            else
+            {
+                busNumber = totalNumber / busCapacity + 1;
+
+
+                Console.WriteLine("You need " + busNumber + " " + busType + " buses.");
+            }
+        }
+    }
+}
+
